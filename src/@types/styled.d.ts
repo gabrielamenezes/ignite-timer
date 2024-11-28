752c37c8eae56abc32b235ca128aa9d9d61b5545
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-28 20:31:37 -0300

    feat: typed props for styled components

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme
+
+//criando uma tipagem para o modulo do styled components
+//toda vez que o styled-components for importado, a definição de tipos será a que eu definir aqui
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}
