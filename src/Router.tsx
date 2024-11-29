f2017891fe283c4f2b996ee2589c7118e40f2d31
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-29 08:34:03 -0300

    feat: defining default layout for routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" Component={Home} />
-      <Route path="/history" Component={History} />
+      <Route path='/' Component={DefaultLayout}>
+        <Route path="/" Component={Home} />
+        <Route path="/history" Component={History} />
+      </Route>
     </Routes>
   )
 }
