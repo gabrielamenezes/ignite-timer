ab2853daaad539f6cf4a9029634b8bb0a56bc5de
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-29 06:57:48 -0300

    feat: add react router dom

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/history" Component={History} />
+    </Routes>
+  )
+}
