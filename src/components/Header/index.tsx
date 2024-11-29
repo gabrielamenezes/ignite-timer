68465660768ebea3e93c23f0ce7196e14a3b46f0
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-29 20:25:20 -0300

    add NavLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import { HeaderContainer } from "./styles"
 import { Scroll, Timer } from "@phosphor-icons/react"
 import logoIgnite  from '../../assets/logo.svg'
+import { NavLink } from "react-router-dom"
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <img src={logoIgnite} alt="Logo da aplicação em tons de verde" />
       <nav>
-        <a href="" title="timer"><Timer /></a>
-        <a href="" title="histórico"><Scroll /></a>
+        <NavLink to='/' title="timer"><Timer /></NavLink>
+        <NavLink to='/history' title="histórico"><Scroll /></NavLink>
       </nav>
     </HeaderContainer>
   )
