fb50752937a703c6ec72d579205070c5f47ab158
Gabriela Menezes <gabimenezess10@gmail.com>
2024-11-29 21:01:26 -0300

    style: add home (form and timer) styles

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Play } from "@phosphor-icons/react"
+import { CountDownContainer, FormContainer, HomeContainer, Separator } from "./styles"
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+        
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={24}/>
+          Começar
+        </button>
+      </form>
+
+    </HomeContainer>
+  )
+}
