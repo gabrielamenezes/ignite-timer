f803109f868bd81c7e1d308698e2c86f654a7734
Gabriela Menezes <gabimenezess10@gmail.com>
2024-12-01 10:55:14 -0300

    feat: add validations to form
    As we add the lib zod to the project, now we are validating the fields registered before
    We also add thne reset function to clean the form inputs after submit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,30 @@
 import { Play } from "@phosphor-icons/react"
 import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles"
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5, 'O ciclo precisa ser no mínimo de 5 minutos').max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 export const Home = () => {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
+  });
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset();
   }
   const task = watch('task')
   const isSubmitDisabled = !task;
-  
+
   return (
     <HomeContainer>
       <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
@@ -38,7 +52,7 @@ export const Home = () => {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountdownButton type="submit" disabled={!isSubmitDisabled}>
+        <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24}/>
           Começar
         </StartCountdownButton>
