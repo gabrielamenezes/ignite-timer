afa2b1b16c6abe133b5a3bbf06b3d082e5afa746
Gabriela Menezes <gabimenezess10@gmail.com>
2024-12-01 10:53:56 -0300

    refactor: delete comments

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,10 @@
 import config from '@rocketseat/eslint-config/react.mjs'
+
 export default {
-  // Estende a configuração importada
   ...config,
   
-  // Adiciona ou sobrescreve regras
   rules: {
-    // Copia as regras existentes do config importado, se houver
     ...config.rules,
-    
-    // Adiciona ou sobrescreve regras específicas
     "@typescript-eslint/no-empty-object-type": "error",
   }
 }
