e74d21d946d0fbd1d83f0fe4e18ca53419503407
Gabriela Menezes <gabimenezess10@gmail.com>
2024-12-01 11:30:52 -0300

    feat: add activeCycleId state
    We need to memorize the active cycle in the application for use in the history page and also to identify what cycle is running.
    So we create a state that when we start the counter, we set this state to the same id of the cycle. Then, we find who is the active by using a find to search who has the cycle.id equals to activeCycleId
    
    This is a better approach because if  a boolean variable were created to store the active state, we would always have to iterate through the array and keep activating and deactivating cycles.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,13 +3,24 @@ import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, S
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState } from "react"
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod.number().min(5, 'O ciclo precisa ser no mínimo de 5 minutos').max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export const Home = () => {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -19,12 +30,21 @@ export const Home = () => {
   });
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime());
+    const newCycle: Cycle = {
+      id,
+      minutesAmount: data.minutesAmount,
+      task: data.task
+    }
+    setCycles(state => [...state, newCycle])
+    setActiveCycleId(id)
     reset();
   }
+
+  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+  
   const task = watch('task')
   const isSubmitDisabled = !task;
-
   return (
     <HomeContainer>
       <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
