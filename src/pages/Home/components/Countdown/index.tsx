b3238af828e8d70329373089bb6740fca09c4dcc
Gabriela Menezes <gabimenezess10@gmail.com>
2024-12-04 19:34:00 -0300

    feat: separate components
    This commit is separating the components that were only in home.

---
+++ src/pages/Home/components/Countdown/index.tsx
@@ -0,0 +1,48 @@
+import { useContext, useEffect, useState } from 'react'
+import { CountDownContainer, Separator } from './style'
+import { differenceInSeconds } from 'date-fns'
+import { CyclesContext } from '../..'
+
+export const Countdown = () => {
+  const [amoutSecondsPassed, setAmountSecondsPassed] = useState(0)
+  const { activeCycle } = useContext(CyclesContext)
+  const totalSeconds = activeCycle
+    ? activeCycle.minutesAmount * 60
+    : 0
+  useEffect(() => {
+    let interval: number
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(), activeCycle.startDate,
+        )
+        if (secondsDifference >= totalSeconds) {
+          setCycles(state => state.map(cycle => {
+            if (cycle.id === activeCycleId) {
+              return { ...cycle, finishedDate: new Date() }
+            } else {
+              return cycle
+            }
+          }))
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(secondsDifference)
+        }
+      }, 1000)
+    }
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeCycle, totalSeconds, activeCycleId])
+
+  return (
+    <CountDownContainer>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
+      <Separator>:</Separator>
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CountDownContainer>
+  )
+}
